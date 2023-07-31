b593dfd788e4d5d18ece9e8f3b221ae18652b0ab
mattallenn <104325813+mattallenn@users.noreply.github.com>
2023-07-31 19:51:53 -0400

    began added curses functionality

--- cspeed.c
+++ cspeed.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ncurses.h>
+
+#define NUM_WORDS 25
 
 int main() 
 {
-        //Opens file 
+    char text[NUM_WORDS * 12]; //Array of all characters of selected words
+
+    initscr(); //Starts Curses
+    raw(); //Disables line buffering
+    noecho(); //Disables echoing of characters
+
+    
+    //Opens file 
     FILE* file = fopen("1-1000.txt", "r");
     if (file == NULL) {
     
@@ -14,7 +24,7 @@ int main()
     }
 
     // Creates 2d array, num_lines is a row, maxchar is the array or chars to make a string
-    char lines[1000][12];
+    char lines[500][12];
     int line_counter = 0;
     
     //Reads file, adding each word to a 2d array
@@ -27,20 +37,27 @@ int main()
 
         line_counter++;
 
-        if (line_counter >= 1000) {
+        if (line_counter >= 500) {
             break;
         }
     }
     
     //prints random word from array
     srand(time(NULL));
-
-    for (int i = 0; i < 1000; i++) {
-        int random = rand() % 1000;
-        printf("%s ", lines[random]);
+    
+    attron(A_BOLD); //Makes text bold
+    for (int i = 0; i < NUM_WORDS; i++) {
+        int random = rand() % 500;
+        printw("%s ", lines[random]);
     }
-
+    attroff(A_BOLD); //Turns off bold
+    
+    refresh();     
+    getch(); //Waits for user input
+    endwin();              
     //closes out of file
     fclose(file);
+
+    return 0;
 }
 
